da7c331eb9099cf069f5ae88f0c7e9e1ccde0abc
jrickert08 <jrickert08@gmail.com>
2025-06-14 00:59:21 -0400

    Add files via upload

---
+++ player_db.py
@@ -0,0 +1,320 @@
+player_db = {
+  "J. Allen": {
+    "full": "Josh Allen",
+    "team": "BUF",
+    "pos": "QB",
+    "bye": 13
+  },
+  "P. Mahomes": {
+    "full": "Patrick Mahomes",
+    "team": "KC",
+    "pos": "QB",
+    "bye": 10
+  },
+  "J. Herbert": {
+    "full": "Justin Herbert",
+    "team": "LAC",
+    "pos": "QB",
+    "bye": 12
+  },
+  "L. Jackson": {
+    "full": "Lamar Jackson",
+    "team": "BAL",
+    "pos": "QB",
+    "bye": 14
+  },
+  "J. Burrow": {
+    "full": "Joe Burrow",
+    "team": "CIN",
+    "pos": "QB",
+    "bye": 12
+  },
+  "C. Stroud": {
+    "full": "C.J. Stroud",
+    "team": "HOU",
+    "pos": "QB",
+    "bye": 14
+  },
+  "D. Prescott": {
+    "full": "Dak Prescott",
+    "team": "DAL",
+    "pos": "QB",
+    "bye": 7
+  },
+  "B. Purdy": {
+    "full": "Brock Purdy",
+    "team": "SF",
+    "pos": "QB",
+    "bye": 9
+  },
+  "T. Tagovailoa": {
+    "full": "Tua Tagovailoa",
+    "team": "MIA",
+    "pos": "QB",
+    "bye": 6
+  },
+  "K. Murray": {
+    "full": "Kyler Murray",
+    "team": "ARI",
+    "pos": "QB",
+    "bye": 11
+  },
+  "C. McCaffrey": {
+    "full": "Christian McCaffrey",
+    "team": "SF",
+    "pos": "RB",
+    "bye": 9
+  },
+  "B. Robinson": {
+    "full": "Bijan Robinson",
+    "team": "ATL",
+    "pos": "RB",
+    "bye": 12
+  },
+  "J. Taylor": {
+    "full": "Jonathan Taylor",
+    "team": "IND",
+    "pos": "RB",
+    "bye": 14
+  },
+  "S. Barkley": {
+    "full": "Saquon Barkley",
+    "team": "PHI",
+    "pos": "RB",
+    "bye": 5
+  },
+  "R. White": {
+    "full": "Rachaad White",
+    "team": "TB",
+    "pos": "RB",
+    "bye": 11
+  },
+  "T. Etienne": {
+    "full": "Travis Etienne",
+    "team": "JAX",
+    "pos": "RB",
+    "bye": 12
+  },
+  "J. Gibbs": {
+    "full": "Jahmyr Gibbs",
+    "team": "DET",
+    "pos": "RB",
+    "bye": 5
+  },
+  "K. Walker": {
+    "full": "Kenneth Walker",
+    "team": "SEA",
+    "pos": "RB",
+    "bye": 10
+  },
+  "Z. Charbonnet": {
+    "full": "Zach Charbonnet",
+    "team": "SEA",
+    "pos": "RB",
+    "bye": 10
+  },
+  "D. Montgomery": {
+    "full": "David Montgomery",
+    "team": "DET",
+    "pos": "RB",
+    "bye": 5
+  },
+  "J. Jefferson": {
+    "full": "Justin Jefferson",
+    "team": "MIN",
+    "pos": "WR",
+    "bye": 6
+  },
+  "J. Chase": {
+    "full": "Ja'Marr Chase",
+    "team": "CIN",
+    "pos": "WR",
+    "bye": 12
+  },
+  "T. Hill": {
+    "full": "Tyreek Hill",
+    "team": "MIA",
+    "pos": "WR",
+    "bye": 6
+  },
+  "A. Brown": {
+    "full": "A.J. Brown",
+    "team": "PHI",
+    "pos": "WR",
+    "bye": 5
+  },
+  "G. Wilson": {
+    "full": "Garrett Wilson",
+    "team": "NYJ",
+    "pos": "WR",
+    "bye": 12
+  },
+  "S. Diggs": {
+    "full": "Stefon Diggs",
+    "team": "HOU",
+    "pos": "WR",
+    "bye": 14
+  },
+  "C. Olave": {
+    "full": "Chris Olave",
+    "team": "NO",
+    "pos": "WR",
+    "bye": 11
+  },
+  "K. Allen": {
+    "full": "Keenan Allen",
+    "team": "CHI",
+    "pos": "WR",
+    "bye": 5
+  },
+  "M. Pittman": {
+    "full": "Michael Pittman Jr.",
+    "team": "IND",
+    "pos": "WR",
+    "bye": 14
+  },
+  "D. London": {
+    "full": "Drake London",
+    "team": "ATL",
+    "pos": "WR",
+    "bye": 12
+  },
+  "J. Waddle": {
+    "full": "Jaylen Waddle",
+    "team": "MIA",
+    "pos": "WR",
+    "bye": 6
+  },
+  "C. Watson": {
+    "full": "Christian Watson",
+    "team": "GB",
+    "pos": "WR",
+    "bye": 10
+  },
+  "N. Dell": {
+    "full": "Tank Dell",
+    "team": "HOU",
+    "pos": "WR",
+    "bye": 14
+  },
+  "R. Doubs": {
+    "full": "Romeo Doubs",
+    "team": "GB",
+    "pos": "WR",
+    "bye": 10
+  },
+  "T. Lockett": {
+    "full": "Tyler Lockett",
+    "team": "SEA",
+    "pos": "WR",
+    "bye": 10
+  },
+  "T. Kelce": {
+    "full": "Travis Kelce",
+    "team": "KC",
+    "pos": "TE",
+    "bye": 10
+  },
+  "S. LaPorta": {
+    "full": "Sam LaPorta",
+    "team": "DET",
+    "pos": "TE",
+    "bye": 5
+  },
+  "M. Andrews": {
+    "full": "Mark Andrews",
+    "team": "BAL",
+    "pos": "TE",
+    "bye": 14
+  },
+  "D. Kincaid": {
+    "full": "Dalton Kincaid",
+    "team": "BUF",
+    "pos": "TE",
+    "bye": 13
+  },
+  "K. Pitts": {
+    "full": "Kyle Pitts",
+    "team": "ATL",
+    "pos": "TE",
+    "bye": 12
+  },
+  "E. Engram": {
+    "full": "Evan Engram",
+    "team": "JAX",
+    "pos": "TE",
+    "bye": 12
+  },
+  "D. Waller": {
+    "full": "Darren Waller",
+    "team": "NYG",
+    "pos": "TE",
+    "bye": 11
+  },
+  "G. Dulcich": {
+    "full": "Greg Dulcich",
+    "team": "DEN",
+    "pos": "TE",
+    "bye": 14
+  },
+  "R. Rice": {
+    "full": "Rashee Rice",
+    "team": "KC",
+    "pos": "WR",
+    "bye": 10
+  },
+  "J. Dotson": {
+    "full": "Jahan Dotson",
+    "team": "WAS",
+    "pos": "WR",
+    "bye": 14
+  },
+  "Z. Moss": {
+    "full": "Zack Moss",
+    "team": "CIN",
+    "pos": "RB",
+    "bye": 12
+  },
+  "T. Spears": {
+    "full": "Tyjae Spears",
+    "team": "TEN",
+    "pos": "RB",
+    "bye": 5
+  },
+  "K. Herbert": {
+    "full": "Khalil Herbert",
+    "team": "CHI",
+    "pos": "RB",
+    "bye": 5
+  },
+  "R. Bateman": {
+    "full": "Rashod Bateman",
+    "team": "BAL",
+    "pos": "WR",
+    "bye": 14
+  },
+  "J. Meyers": {
+    "full": "Jakobi Meyers",
+    "team": "LV",
+    "pos": "WR",
+    "bye": 10
+  },
+  "Q. Johnston": {
+    "full": "Quentin Johnston",
+    "team": "LAC",
+    "pos": "WR",
+    "bye": 12
+  },
+  "J. Mingo": {
+    "full": "Jonathan Mingo",
+    "team": "CAR",
+    "pos": "WR",
+    "bye": 11
+  },
+  "M. Wilson": {
+    "full": "Michael Wilson",
+    "team": "ARI",
+    "pos": "WR",
+    "bye": 11
+  }
+}
